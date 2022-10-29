4a0b7ba2e3cf2cf9908faee10fd9351b46347ff2
SeungJae <seanexplode@gmail.com>
2022-10-29 20:44:45 +0900

    Do not import non-existing module

--- recbole_debias/model/debiased_recommender/__init__.py
+++ recbole_debias/model/debiased_recommender/__init__.py
@@ -3,7 +3,6 @@ from recbole_debias.model.debiased_recommender.cause import CausE
 from recbole_debias.model.debiased_recommender.dice import DICE
 from recbole_debias.model.debiased_recommender.macr import MACR
 from recbole_debias.model.debiased_recommender.mf_ips import MF_IPS
-from recbole_debias.model.debiased_recommender.neumf import NeuMF
 from recbole_debias.model.debiased_recommender.pda import PDA
 from recbole_debias.model.debiased_recommender.rel_mf import REL_MF
 
